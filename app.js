5414daa13eabb4a5c85e7312c2fc15e48d944026
Chris-Bortel <bortelcc19@gmail.com>
2020-04-08 15:27:10 -0700

    rendering list to dom

--- app.js
+++ app.js
@@ -43,60 +43,10 @@ Store.prototype.totalSales = function () {
   }
 };
 
-
-
-// console.log(Store);
-var seattleStore = new Store('seattle', 23, 65, 6.3);
-console.log(seattleStore);
-seattleStore.totalSales();
-console.log(seattleStore.cookieSalesPerHour);
-console.log(seattleStore.totalSalesPerDay);
-//1 .
-
-//==================================
-
-
-
-var seattle = {
-  storename: 'seattle',
-  //assigning var an object
-  //location: 'Seattle',
-  minCust: 23,
-  maxCust: 65, //could say that we are assigning key value pair
-  avgCookieSale: 6.3,
-  randCustomers: [],
-  cookieSalesPerHour: [], //creating an empty array. called an array literal
-  //picture: put in a picture. assign a striing url
-  totalSalesPerDay: 0,
-  storeHoursOpen: [ '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',],
-};
-//=========================================================
-// 2. this is a method of seattle so 'this' refers to seattle
-// Dynamic randomizer
-seattle.randCustomers = function () {
-  return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
-};
-
-
-//==========================================================
-// 3. New Method
-seattle.totalSales = function () {
-  for (var i = 0; i < this.storeHoursOpen.length; i++) {
-    var customersPerHour = this.randCustomers();
-    var currentcookieSalesPerHour = this.avgCookieSale * customersPerHour;
-    this.cookieSalesPerHour.push(Math.floor(currentcookieSalesPerHour));
-    //for each hour output a num of cookies
-  }
-  //totalSalesPerDay represents the bucket of cookies
-  for (i = 0; i < this.cookieSalesPerHour.length; i++){
-    this.totalSalesPerDay = this.totalSalesPerDay + this.cookieSalesPerHour[i];
-  }
-};
-
 //=========================================================
 //Method to Render
 
-seattle.renderToPage = function(){
+Store.prototype.renderToPage = function(){
   //1. find target
   var targetUlEl = document.getElementById('seattlePH');
   //2. create content
@@ -113,117 +63,189 @@ seattle.renderToPage = function(){
   newLiEl.textContent = renderedItems;// we are assigning the text content of our newLiEl to be the string renderedItems 
   //3. append to target
   targetUlEl.appendChild(newLiEl)
-}
-seattle.totalSales();
-seattle.renderToPage();
+};
 
 
-
-
-//=============================================================
-//=============================================================
-
+// console.log(Store);
+var seattleStore = new Store('seattle', 23, 65, 6.3);
+console.log(seattleStore);
+seattleStore.totalSales();
+seattleStore.renderToPage();
+console.log(seattleStore.cookieSalesPerHour);
+console.log(seattleStore.totalSalesPerDay);
 //1 .
-var tokyo = {
-  storename: 'tokyo',
-  //assigning var an object
-  //location: 'tokyo',
-  minCust: 23,
-  maxCust: 65, //could say that we are assigning key value pair
-  avgCookieSale: 6.3,
-  randCustomers: [],
-  cookieSalesPerHour: [], //creating an empty array. called an array literal
-  //picture: put in a picture. assign a striing url
-  storeHoursOpen: [ '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',],
-};
-//=========================================================
-// 2. this is a method of tokyo so 'this' refers to tokyo
-// Dynamic randomizer
-tokyo.randCustomers = function () {
-  return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
-};
 
-//==========================================================
-// 3. New Method
-tokyo.totalSales = function () {
-  for (var i = 0; i < this.storeHoursOpen.length; i++) {
-    var customersPerHour = this.randCustomers();
-    var cookieSalesPerHour = this.avgCookieSale * customersPerHour;
-    this.cookieSalesPerHour.push(Math.floor(cookieSalesPerHour));
-    //for each hour output a num of cookies
-  }
-};
-
-//=========================================================
-//Method to Render
-
-tokyo.renderToPage = function(){
-  //1. find target
-  var targetUlEl = document.getElementById('tokyoPH');
-  //2. create content
-  for(var i=0 ; i < this.storeHoursOpen.length; i++){
-    var newLiEl = document.createElement('li');
-    var renderedItems =this.storeHoursOpen[i] + ': ' + this.cookieSalesPerHour[i] + ' cookies';// recreating and reassigning var render
-    newLiEl.textContent = renderedItems;// we are assigning the text content of our newLiEl to be the string renderedItems 
-    //3. append to target
-    targetUlEl.appendChild(newLiEl)
-  }
-}
-tokyo.totalSales();
-tokyo.renderToPage();
+//==================================
 
 
 
-//=============================================================
-//=============================================================
+// var seattle = {
+//   storename: 'seattle',
+//   //assigning var an object
+//   //location: 'Seattle',
+//   minCust: 23,
+//   maxCust: 65, //could say that we are assigning key value pair
+//   avgCookieSale: 6.3,
+//   randCustomers: [],
+//   cookieSalesPerHour: [], //creating an empty array. called an array literal
+//   //picture: put in a picture. assign a striing url
+//   totalSalesPerDay: 0,
+//   storeHoursOpen: [ '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',],
+// };
+// //=========================================================
+// // 2. this is a method of seattle so 'this' refers to seattle
+// // Dynamic randomizer
+// seattle.randCustomers = function () {
+//   return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+// };
 
 
-//1 .
-var dubai = {
-  storename: 'dubai',
-  //assigning var an object
-  //location: 'dubai',
-  minCust: 23,
-  maxCust: 65, //could say that we are assigning key value pair
-  avgCookieSale: 6.3,
-  randCustomers: [],
-  cookieSalesPerHour: [], //creating an empty array. called an array literal
-  //picture: put in a picture. assign a striing url
-  storeHoursOpen: [ '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',],
-};
-//=========================================================
-// 2. this is a method of dubai so 'this' refers to dubai
-// Dynamic randomizer
-dubai.randCustomers = function () {
-  return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
-};
+// //==========================================================
+// // 3. New Method
+// seattle.totalSales = function () {
+//   for (var i = 0; i < this.storeHoursOpen.length; i++) {
+//     var customersPerHour = this.randCustomers();
+//     var currentcookieSalesPerHour = this.avgCookieSale * customersPerHour;
+//     this.cookieSalesPerHour.push(Math.floor(currentcookieSalesPerHour));
+//     //for each hour output a num of cookies
+//   }
+//   //totalSalesPerDay represents the bucket of cookies
+//   for (i = 0; i < this.cookieSalesPerHour.length; i++){
+//     this.totalSalesPerDay = this.totalSalesPerDay + this.cookieSalesPerHour[i];
+//   }
+// };
 
-//==========================================================
-// 3. New Method
-dubai.totalSales = function () {
-  for (var i = 0; i < this.storeHoursOpen.length; i++) {
-    var customersPerHour = this.randCustomers();
-    var cookieSalesPerHour = this.avgCookieSale * customersPerHour;
-    this.cookieSalesPerHour.push(Math.floor(cookieSalesPerHour));
-    //for each hour output a num of cookies
-  }
-};
+// //=========================================================
+// //Method to Render
 
-//=========================================================
-//Method to Render
+// seattle.renderToPage = function(){
+//   //1. find target
+//   var targetUlEl = document.getElementById('seattlePH');
+//   //2. create content
+//   for(var i=0 ; i < this.storeHoursOpen.length; i++){
+//     var newLiEl = document.createElement('li');
+//     var renderedItems =this.storeHoursOpen[i] + ': ' + this.cookieSalesPerHour[i] + ' cookies';// recreating and reassigning var render
+//     newLiEl.textContent = renderedItems;// we are assigning the text content of our newLiEl to be the string renderedItems 
+//     //3. append to target
+//     targetUlEl.appendChild(newLiEl)
+//   }
+//   //used attribute total sales tally from the old function and created a newLiEl, reassigned its text content, and then  rendered to the dom 
+//   newLiEl = document.createElement('li');
+//   renderedItems ='Total: ' + this.totalSalesPerDay + ' cookies';// recreating and reassigning var render
+//   newLiEl.textContent = renderedItems;// we are assigning the text content of our newLiEl to be the string renderedItems 
+//   //3. append to target
+//   targetUlEl.appendChild(newLiEl)
+// }
+// seattle.totalSales();
+// seattle.renderToPage();
 
-dubai.renderToPage = function(){
-  //1. find target
-  var targetUlEl = document.getElementById('dubaiPH');
-  //2. create content
-  for(var i=0 ; i < this.storeHoursOpen.length; i++){
-    var newLiEl = document.createElement('li');
-    var renderedItems =this.storeHoursOpen[i] + ': ' + this.cookieSalesPerHour[i] + ' cookies';// recreating and reassigning var render
-    newLiEl.textContent = renderedItems;// we are assigning the text content of our newLiEl to be the string renderedItems 
-    //3. append to target
-    targetUlEl.appendChild(newLiEl)
-  }
-}
-dubai.totalSales();
-dubai.renderToPage();
+
+
+
+// //=============================================================
+// //=============================================================
+
+// //1 .
+// var tokyo = {
+//   storename: 'tokyo',
+//   //assigning var an object
+//   //location: 'tokyo',
+//   minCust: 23,
+//   maxCust: 65, //could say that we are assigning key value pair
+//   avgCookieSale: 6.3,
+//   randCustomers: [],
+//   cookieSalesPerHour: [], //creating an empty array. called an array literal
+//   //picture: put in a picture. assign a striing url
+//   storeHoursOpen: [ '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',],
+// };
+// //=========================================================
+// // 2. this is a method of tokyo so 'this' refers to tokyo
+// // Dynamic randomizer
+// tokyo.randCustomers = function () {
+//   return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+// };
+
+// //==========================================================
+// // 3. New Method
+// tokyo.totalSales = function () {
+//   for (var i = 0; i < this.storeHoursOpen.length; i++) {
+//     var customersPerHour = this.randCustomers();
+//     var cookieSalesPerHour = this.avgCookieSale * customersPerHour;
+//     this.cookieSalesPerHour.push(Math.floor(cookieSalesPerHour));
+//     //for each hour output a num of cookies
+//   }
+// };
+
+// //=========================================================
+// //Method to Render
+
+// tokyo.renderToPage = function(){
+//   //1. find target
+//   var targetUlEl = document.getElementById('tokyoPH');
+//   //2. create content
+//   for(var i=0 ; i < this.storeHoursOpen.length; i++){
+//     var newLiEl = document.createElement('li');
+//     var renderedItems =this.storeHoursOpen[i] + ': ' + this.cookieSalesPerHour[i] + ' cookies';// recreating and reassigning var render
+//     newLiEl.textContent = renderedItems;// we are assigning the text content of our newLiEl to be the string renderedItems 
+//     //3. append to target
+//     targetUlEl.appendChild(newLiEl)
+//   }
+// }
+// tokyo.totalSales();
+// tokyo.renderToPage();
+
+
+
+// //=============================================================
+// //=============================================================
+
+
+// //1 .
+// var dubai = {
+//   storename: 'dubai',
+//   //assigning var an object
+//   //location: 'dubai',
+//   minCust: 23,
+//   maxCust: 65, //could say that we are assigning key value pair
+//   avgCookieSale: 6.3,
+//   randCustomers: [],
+//   cookieSalesPerHour: [], //creating an empty array. called an array literal
+//   //picture: put in a picture. assign a striing url
+//   storeHoursOpen: [ '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',],
+// };
+// //=========================================================
+// // 2. this is a method of dubai so 'this' refers to dubai
+// // Dynamic randomizer
+// dubai.randCustomers = function () {
+//   return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+// };
+
+// //==========================================================
+// // 3. New Method
+// dubai.totalSales = function () {
+//   for (var i = 0; i < this.storeHoursOpen.length; i++) {
+//     var customersPerHour = this.randCustomers();
+//     var cookieSalesPerHour = this.avgCookieSale * customersPerHour;
+//     this.cookieSalesPerHour.push(Math.floor(cookieSalesPerHour));
+//     //for each hour output a num of cookies
+//   }
+// };
+
+// //=========================================================
+// //Method to Render
+
+// dubai.renderToPage = function(){
+//   //1. find target
+//   var targetUlEl = document.getElementById('dubaiPH');
+//   //2. create content
+//   for(var i=0 ; i < this.storeHoursOpen.length; i++){
+//     var newLiEl = document.createElement('li');
+//     var renderedItems =this.storeHoursOpen[i] + ': ' + this.cookieSalesPerHour[i] + ' cookies';// recreating and reassigning var render
+//     newLiEl.textContent = renderedItems;// we are assigning the text content of our newLiEl to be the string renderedItems 
+//     //3. append to target
+//     targetUlEl.appendChild(newLiEl)
+//   }
+// }
+// dubai.totalSales();
+// dubai.renderToPage();
 
