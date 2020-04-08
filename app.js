56c5690a8c66267a8b20c21413c950a2389193ce
Chris-Bortel <bortelcc19@gmail.com>
2020-04-08 15:19:11 -0700

    constructor function is working with my randomizer, total sales function. Still needs to be rendered.

--- app.js
+++ app.js
@@ -5,19 +5,67 @@
 
 /**
  * //What do we need to be doing?
+ * //I want to build a constructor function that will build my store objects
+ * //data needed for fucntion Store(minCust, maxCust, storestorename, avgCookieSale, cookieSalesPerHour, totalSales)
  */
 
+//constructor function--- Pulling the properties into the object to be rendered... It is making the object
+//TODO: This is where we will put in our store data, and the Store constructor function will return our object
+function Store(storeName, minCust, maxCust, avgCookieSale) {
+  //stuff being constructed from the store data
+  this.storeName = storeName;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookieSale = avgCookieSale;
+  // this.randCustomersArray = [];// Do I need this anymore?---- It was the product on the rand customer function, and we are going to put min and maxCust directly into the randCustomers function. 
+  
+  this.storeHoursOpen = [ '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',];
+  this.cookieSalesPerHour = []; //this is because this empty array is waiting for sales to be pushed into it
+  this.totalSalesPerDay = 0; //this is the value that totalSalesPerDay will be each day
+}
 
+//Randomizer====================
+Store.prototype.randCustomers = function () {
+  return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+};
+
+// totalSales function+==================
+Store.prototype.totalSales = function () {
+  for (var i = 0; i < this.storeHoursOpen.length; i++) {
+    var customersPerHour = this.randCustomers();
+    var currentcookieSalesPerHour = this.avgCookieSale * customersPerHour;
+    this.cookieSalesPerHour.push(Math.floor(currentcookieSalesPerHour));
+    //for each hour output a num of cookies
+  }
+  //totalSalesPerDay represents the bucket of cookies
+  for (i = 0; i < this.cookieSalesPerHour.length; i++){
+    this.totalSalesPerDay = this.totalSalesPerDay + this.cookieSalesPerHour[i];
+  }
+};
+
+
+
+// console.log(Store);
+var seattleStore = new Store('seattle', 23, 65, 6.3);
+console.log(seattleStore);
+seattleStore.totalSales();
+console.log(seattleStore.cookieSalesPerHour);
+console.log(seattleStore.totalSalesPerDay);
 //1 .
+
+//==================================
+
+
+
 var seattle = {
-  name: 'seattle',
+  storename: 'seattle',
   //assigning var an object
   //location: 'Seattle',
   minCust: 23,
   maxCust: 65, //could say that we are assigning key value pair
   avgCookieSale: 6.3,
   randCustomers: [],
-  cookiesPerHour: [], //creating an empty array. called an array literal
+  cookieSalesPerHour: [], //creating an empty array. called an array literal
   //picture: put in a picture. assign a striing url
   totalSalesPerDay: 0,
   storeHoursOpen: [ '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',],
@@ -29,18 +77,19 @@ seattle.randCustomers = function () {
   return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
 };
 
+
 //==========================================================
 // 3. New Method
 seattle.totalSales = function () {
   for (var i = 0; i < this.storeHoursOpen.length; i++) {
     var customersPerHour = this.randCustomers();
-    var currentCookiesPerHour = this.avgCookieSale * customersPerHour;
-    this.cookiesPerHour.push(Math.floor(currentCookiesPerHour));
+    var currentcookieSalesPerHour = this.avgCookieSale * customersPerHour;
+    this.cookieSalesPerHour.push(Math.floor(currentcookieSalesPerHour));
     //for each hour output a num of cookies
   }
   //totalSalesPerDay represents the bucket of cookies
-  for (i = 0; i < this.cookiesPerHour.length; i++){
-    this.totalSalesPerDay = this.totalSalesPerDay + this.cookiesPerHour[i];
+  for (i = 0; i < this.cookieSalesPerHour.length; i++){
+    this.totalSalesPerDay = this.totalSalesPerDay + this.cookieSalesPerHour[i];
   }
 };
 
@@ -53,7 +102,7 @@ seattle.renderToPage = function(){
   //2. create content
   for(var i=0 ; i < this.storeHoursOpen.length; i++){
     var newLiEl = document.createElement('li');
-    var renderedItems =this.storeHoursOpen[i] + ': ' + this.cookiesPerHour[i] + ' cookies';// recreating and reassigning var render
+    var renderedItems =this.storeHoursOpen[i] + ': ' + this.cookieSalesPerHour[i] + ' cookies';// recreating and reassigning var render
     newLiEl.textContent = renderedItems;// we are assigning the text content of our newLiEl to be the string renderedItems 
     //3. append to target
     targetUlEl.appendChild(newLiEl)
@@ -76,14 +125,14 @@ seattle.renderToPage();
 
 //1 .
 var tokyo = {
-  name: 'tokyo',
+  storename: 'tokyo',
   //assigning var an object
   //location: 'tokyo',
   minCust: 23,
   maxCust: 65, //could say that we are assigning key value pair
   avgCookieSale: 6.3,
   randCustomers: [],
-  cookiesPerHour: [], //creating an empty array. called an array literal
+  cookieSalesPerHour: [], //creating an empty array. called an array literal
   //picture: put in a picture. assign a striing url
   storeHoursOpen: [ '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',],
 };
@@ -99,8 +148,8 @@ tokyo.randCustomers = function () {
 tokyo.totalSales = function () {
   for (var i = 0; i < this.storeHoursOpen.length; i++) {
     var customersPerHour = this.randCustomers();
-    var cookiesPerHour = this.avgCookieSale * customersPerHour;
-    this.cookiesPerHour.push(Math.floor(cookiesPerHour));
+    var cookieSalesPerHour = this.avgCookieSale * customersPerHour;
+    this.cookieSalesPerHour.push(Math.floor(cookieSalesPerHour));
     //for each hour output a num of cookies
   }
 };
@@ -114,7 +163,7 @@ tokyo.renderToPage = function(){
   //2. create content
   for(var i=0 ; i < this.storeHoursOpen.length; i++){
     var newLiEl = document.createElement('li');
-    var renderedItems =this.storeHoursOpen[i] + ': ' + this.cookiesPerHour[i] + ' cookies';// recreating and reassigning var render
+    var renderedItems =this.storeHoursOpen[i] + ': ' + this.cookieSalesPerHour[i] + ' cookies';// recreating and reassigning var render
     newLiEl.textContent = renderedItems;// we are assigning the text content of our newLiEl to be the string renderedItems 
     //3. append to target
     targetUlEl.appendChild(newLiEl)
@@ -131,14 +180,14 @@ tokyo.renderToPage();
 
 //1 .
 var dubai = {
-  name: 'dubai',
+  storename: 'dubai',
   //assigning var an object
   //location: 'dubai',
   minCust: 23,
   maxCust: 65, //could say that we are assigning key value pair
   avgCookieSale: 6.3,
   randCustomers: [],
-  cookiesPerHour: [], //creating an empty array. called an array literal
+  cookieSalesPerHour: [], //creating an empty array. called an array literal
   //picture: put in a picture. assign a striing url
   storeHoursOpen: [ '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',],
 };
@@ -154,8 +203,8 @@ dubai.randCustomers = function () {
 dubai.totalSales = function () {
   for (var i = 0; i < this.storeHoursOpen.length; i++) {
     var customersPerHour = this.randCustomers();
-    var cookiesPerHour = this.avgCookieSale * customersPerHour;
-    this.cookiesPerHour.push(Math.floor(cookiesPerHour));
+    var cookieSalesPerHour = this.avgCookieSale * customersPerHour;
+    this.cookieSalesPerHour.push(Math.floor(cookieSalesPerHour));
     //for each hour output a num of cookies
   }
 };
@@ -169,7 +218,7 @@ dubai.renderToPage = function(){
   //2. create content
   for(var i=0 ; i < this.storeHoursOpen.length; i++){
     var newLiEl = document.createElement('li');
-    var renderedItems =this.storeHoursOpen[i] + ': ' + this.cookiesPerHour[i] + ' cookies';// recreating and reassigning var render
+    var renderedItems =this.storeHoursOpen[i] + ': ' + this.cookieSalesPerHour[i] + ' cookies';// recreating and reassigning var render
     newLiEl.textContent = renderedItems;// we are assigning the text content of our newLiEl to be the string renderedItems 
     //3. append to target
     targetUlEl.appendChild(newLiEl)
